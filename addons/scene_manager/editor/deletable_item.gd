dfcdacd91b118ae9ba9f2003fb07f72dae6c5c49
Degarashi <degarasu@gmail.com>
2026-01-28 00:50:00 +0900

    [update] Modernize node access and signal emission
    Streamline node property access using Godot 4's '%' shorthand. Refactor signal emission to direct '.emit()' method.

--- addons/scene_manager/editor/deletable_item.gd
+++ addons/scene_manager/editor/deletable_item.gd
@@ -7,9 +7,6 @@ const F = preload("uid://cpxe18s2130m8")
 ## Reference to the manager root node
 var _root: SMgrManager
 
-## Internal node references
-@onready var _address_label: LineEdit = %address
-
 
 func _ready() -> void:
 	_root = F.find_manager_root(self)
@@ -17,20 +14,15 @@ func _ready() -> void:
 
 ## Set address and update node name
 func set_address(addr: String) -> void:
-	get_node("%address").text = addr
+	%address.text = addr
 	name = addr
 
 
 ## Return the current address text
 func get_address() -> String:
-	return _address_label.text
+	return %address.text
 
 
 ## Notify the root manager to handle deletion via signal
 func _on_remove_button_up() -> void:
-	const SIG_NAME = "include_child_deleted"
-	if _root and _root.has_signal(SIG_NAME):
-		_root.emit_signal(SIG_NAME, self, get_address())
-	else:
-		# Fallback if root is not found
-		queue_free()
+	_root.include_child_deleted.emit(self, get_address())
