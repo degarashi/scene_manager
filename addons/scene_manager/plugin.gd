89a5b87d7bb9ededc0fdc24bf9d7edfcbca65b35
Degarashi <degarasu@gmail.com>
2026-01-28 12:56:08 +0900

    [update] Main panel variable and constants update
    Renaming of `_menu` variable to `_main_panel` for enhanced clarity.
    Introduction of `MAIN_PANEL_SCENE` and `MAIN_PANEL_NAME` constants for main panel management.
    Addition of return type hints and consistent code formatting.

--- addons/scene_manager/plugin.gd
+++ addons/scene_manager/plugin.gd
@@ -1,51 +1,83 @@
 @tool
 extends EditorPlugin
 
-var _menu: Node
+const MAIN_PANEL_SCENE = preload("uid://crnf0w0s44hxx")
+const MAIN_PANEL_NAME = "Scene Manager"
+var _main_panel: SMgrMainPanel
 var _plugin: Object
 
 
-func set_properties_for_setting():
-	ProjectSettings.add_property_info({
-		"name": SceneManagerConstants.SETTINGS_SCENE_PROPERTY_NAME,
-		"type": TYPE_STRING,
-		"hint": PROPERTY_HINT_FILE,
-		"hint_string": "scenes.gd"
-	})
-	
-	ProjectSettings.add_property_info({
-		"name": SceneManagerConstants.SETTINGS_FADE_OUT_PROPERTY_NAME,
-		"type": TYPE_FLOAT,
-	})
+func set_properties_for_setting() -> void:
+	ProjectSettings.add_property_info(
+		{
+			"name": SceneManagerConstants.SETTINGS_SCENE_PROPERTY_NAME,
+			"type": TYPE_STRING,
+			"hint": PROPERTY_HINT_FILE,
+			"hint_string": "scenes.gd"
+		}
+	)
 
-	ProjectSettings.add_property_info({
-		"name": SceneManagerConstants.SETTINGS_FADE_IN_PROPERTY_NAME,
-		"type": TYPE_FLOAT,
-	})
+	(
+		ProjectSettings
+		. add_property_info(
+			{
+				"name": SceneManagerConstants.SETTINGS_FADE_OUT_PROPERTY_NAME,
+				"type": TYPE_FLOAT,
+			}
+		)
+	)
 
-	ProjectSettings.add_property_info({
-		"name": SceneManagerConstants.SETTINGS_AUTO_SAVE_PROPERTY_NAME,
-		"type": TYPE_BOOL,
-	})
+	(
+		ProjectSettings
+		. add_property_info(
+			{
+				"name": SceneManagerConstants.SETTINGS_FADE_IN_PROPERTY_NAME,
+				"type": TYPE_FLOAT,
+			}
+		)
+	)
 
-	ProjectSettings.set_initial_value(SceneManagerConstants.SETTINGS_SCENE_PROPERTY_NAME, SceneManagerConstants.DEFAULT_PATH_TO_SCENES)
+	(
+		ProjectSettings
+		. add_property_info(
+			{
+				"name": SceneManagerConstants.SETTINGS_AUTO_SAVE_PROPERTY_NAME,
+				"type": TYPE_BOOL,
+			}
+		)
+	)
+
+	ProjectSettings.set_initial_value(
+		SceneManagerConstants.SETTINGS_SCENE_PROPERTY_NAME,
+		SceneManagerConstants.DEFAULT_PATH_TO_SCENES
+	)
 	ProjectSettings.set_as_basic(SceneManagerConstants.SETTINGS_SCENE_PROPERTY_NAME, true)
-	
-	ProjectSettings.set_initial_value(SceneManagerConstants.SETTINGS_FADE_OUT_PROPERTY_NAME, SceneManagerConstants.DEFAULT_FADE_OUT_TIME)
+
+	ProjectSettings.set_initial_value(
+		SceneManagerConstants.SETTINGS_FADE_OUT_PROPERTY_NAME,
+		SceneManagerConstants.DEFAULT_FADE_OUT_TIME
+	)
 	ProjectSettings.set_as_basic(SceneManagerConstants.SETTINGS_FADE_OUT_PROPERTY_NAME, true)
 
-	ProjectSettings.set_initial_value(SceneManagerConstants.SETTINGS_FADE_IN_PROPERTY_NAME, SceneManagerConstants.DEFAULT_FADE_IN_TIME)
+	ProjectSettings.set_initial_value(
+		SceneManagerConstants.SETTINGS_FADE_IN_PROPERTY_NAME,
+		SceneManagerConstants.DEFAULT_FADE_IN_TIME
+	)
 	ProjectSettings.set_as_basic(SceneManagerConstants.SETTINGS_FADE_IN_PROPERTY_NAME, true)
 
 	ProjectSettings.set_initial_value(SceneManagerConstants.SETTINGS_AUTO_SAVE_PROPERTY_NAME, false)
 	ProjectSettings.set_as_internal(SceneManagerConstants.SETTINGS_AUTO_SAVE_PROPERTY_NAME, true)
 
-	ProjectSettings.set_initial_value(SceneManagerConstants.SETTINGS_INCLUDES_VISIBLE_PROPERTY_NAME, true)
-	ProjectSettings.set_as_internal(SceneManagerConstants.SETTINGS_INCLUDES_VISIBLE_PROPERTY_NAME, true)
+	ProjectSettings.set_initial_value(
+		SceneManagerConstants.SETTINGS_INCLUDES_VISIBLE_PROPERTY_NAME, true
+	)
+	ProjectSettings.set_as_internal(
+		SceneManagerConstants.SETTINGS_INCLUDES_VISIBLE_PROPERTY_NAME, true
+	)
 
 	# Restart is required as path to Scenes singleton has changed
 	ProjectSettings.set_restart_if_changed(SceneManagerConstants.SETTINGS_SCENE_PROPERTY_NAME, true)
-	
+
 	ProjectSettings.save()
 
 
@@ -53,37 +85,55 @@ func set_properties_for_setting():
 func _enter_tree():
 	# Adding settings property to Project/Settings & loading
 	if not ProjectSettings.has_setting(SceneManagerConstants.SETTINGS_SCENE_PROPERTY_NAME):
-		ProjectSettings.set_setting(SceneManagerConstants.SETTINGS_SCENE_PROPERTY_NAME, SceneManagerConstants.DEFAULT_PATH_TO_SCENES)
-	
+		ProjectSettings.set_setting(
+			SceneManagerConstants.SETTINGS_SCENE_PROPERTY_NAME,
+			SceneManagerConstants.DEFAULT_PATH_TO_SCENES
+		)
+
 	if not ProjectSettings.has_setting(SceneManagerConstants.SETTINGS_FADE_OUT_PROPERTY_NAME):
-		ProjectSettings.set_setting(SceneManagerConstants.SETTINGS_FADE_OUT_PROPERTY_NAME, SceneManagerConstants.DEFAULT_FADE_OUT_TIME)
+		ProjectSettings.set_setting(
+			SceneManagerConstants.SETTINGS_FADE_OUT_PROPERTY_NAME,
+			SceneManagerConstants.DEFAULT_FADE_OUT_TIME
+		)
 
 	if not ProjectSettings.has_setting(SceneManagerConstants.SETTINGS_FADE_IN_PROPERTY_NAME):
-		ProjectSettings.set_setting(SceneManagerConstants.SETTINGS_FADE_IN_PROPERTY_NAME, SceneManagerConstants.DEFAULT_FADE_IN_TIME)
-	
+		ProjectSettings.set_setting(
+			SceneManagerConstants.SETTINGS_FADE_IN_PROPERTY_NAME,
+			SceneManagerConstants.DEFAULT_FADE_IN_TIME
+		)
+
 	if not ProjectSettings.has_setting(SceneManagerConstants.SETTINGS_AUTO_SAVE_PROPERTY_NAME):
 		ProjectSettings.set_setting(SceneManagerConstants.SETTINGS_AUTO_SAVE_PROPERTY_NAME, false)
 
-	if not ProjectSettings.has_setting(SceneManagerConstants.SETTINGS_INCLUDES_VISIBLE_PROPERTY_NAME):
-		ProjectSettings.set_setting(SceneManagerConstants.SETTINGS_INCLUDES_VISIBLE_PROPERTY_NAME, true)
-	
+	if not ProjectSettings.has_setting(
+		SceneManagerConstants.SETTINGS_INCLUDES_VISIBLE_PROPERTY_NAME
+	):
+		ProjectSettings.set_setting(
+			SceneManagerConstants.SETTINGS_INCLUDES_VISIBLE_PROPERTY_NAME, true
+		)
+
 	set_properties_for_setting()
 
-	add_custom_type("Auto Complete Assistant", 
-			"Node",
-			preload("res://addons/scene_manager/auto_complete_menu_node/scripts/auto_complete_assistant.gd"),
-			preload("res://addons/scene_manager/icons/line-edit-complete-icon.svg"))
+	add_custom_type(
+		"Auto Complete Assistant",
+		"Node",
+		preload(
+			"res://addons/scene_manager/auto_complete_menu_node/scripts/auto_complete_assistant.gd"
+		),
+		preload("res://addons/scene_manager/icons/line-edit-complete-icon.svg")
+	)
 
-	_menu = preload("uid://crnf0w0s44hxx").instantiate()
-	_menu.name = "Scene Manager"
-	add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_UL, _menu)
+	# --- main panel ---
+	_main_panel = MAIN_PANEL_SCENE.instantiate()
+	_main_panel.name = MAIN_PANEL_NAME
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_UL, _main_panel)
 
 	_plugin = preload("res://addons/scene_manager/property_editor/scene_inspector_plugin.gd").new()
 	add_inspector_plugin(_plugin)
 
 
 # Plugin uninstallation
-func _exit_tree():
+func _exit_tree() -> void:
 	# TODO: We can use this function but it removes the saved value of it
 	# along side with the gui setting, if you want to actually just
 	# restart the plugin, you have to set the value for scenes path again
@@ -94,21 +144,23 @@ func _exit_tree():
 	#
 	# We just don't remove the settings for now
 	remove_custom_type("Auto Complete Assistant")
-	remove_control_from_docks(_menu)
-	_menu.free()
+	remove_control_from_docks(_main_panel)
+	_main_panel.free()
 
 	remove_inspector_plugin(_plugin)
 
 
-func _enable_plugin():
-	var path_to_scenes = SceneManagerConstants.DEFAULT_PATH_TO_SCENES
+func _enable_plugin() -> void:
+	var path_to_scenes := SceneManagerConstants.DEFAULT_PATH_TO_SCENES
 	if ProjectSettings.has_setting(SceneManagerConstants.SETTINGS_SCENE_PROPERTY_NAME):
-		path_to_scenes = ProjectSettings.get_setting(SceneManagerConstants.SETTINGS_SCENE_PROPERTY_NAME)
-	
+		path_to_scenes = ProjectSettings.get_setting(
+			SceneManagerConstants.SETTINGS_SCENE_PROPERTY_NAME
+		)
+
 	add_autoload_singleton("SceneManager", "res://addons/scene_manager/scene_manager.tscn")
 	add_autoload_singleton("Scenes", path_to_scenes)
 
 
-func _disable_plugin():
+func _disable_plugin() -> void:
 	remove_autoload_singleton("SceneManager")
 	remove_autoload_singleton("Scenes")
