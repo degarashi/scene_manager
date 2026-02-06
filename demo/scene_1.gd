a4f35e7310324d164d0be3cb4177008571125518
Degarashi <degarasu@gmail.com>
2026-02-06 20:52:44 +0900

    [add] Scene reload and quit functionality
    Implement scene reload and game exit functions. Introduce dedicated buttons in Scene 1 for these actions, organizing them within a VBoxContainer. Standardize quit button text in Scene 0.

--- demo/scene_1.gd
+++ demo/scene_1.gd
@@ -3,3 +3,11 @@ extends Control
 
 func _on_load_scene_2_button_button_up() -> void:
 	SceneManager.switch_to_scene(Scenes.Id.SCENE_2)
+
+
+func _on_reload_button_button_up() -> void:
+	SceneManager.reload_current_scene()
+
+
+func _on_quit_button_button_up() -> void:
+	SceneManager.exit_game()
