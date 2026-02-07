7bfcb994d70822dd772b5f085078825094badb82
Degarashi <degarasu@gmail.com>
2026-02-08 01:24:02 +0900

    [update] Documentation comment formatting
    Removal of `[br]` tags from GDScript documentation comments.

--- addons/scene_manager/scene_manager_constants.gd
+++ addons/scene_manager/scene_manager_constants.gd
@@ -1,11 +1,11 @@
 class_name SMgrConstants
 ## Constants used by the editor and the scene manager in the game
 
-## Enums for how to load the scene.[br]
+## Enums for how to load the scene.
 enum SceneLoadingMode {
-	## Will make it so only one scene will exist for the whole tree. Default option.[br]
+	## Will make it so only one scene will exist for the whole tree. Default option.
 	SINGLE,
-	## Will make it so only one scene will exist for the specified node.[br]
+	## Will make it so only one scene will exist for the specified node.
 	SINGLE_NODE,
 	## Will add the scene to the node along with anything else loaded.
 	ADDITIVE
