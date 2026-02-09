08c07a06963ec20e06502647389a3eec6c8e6670
Degarashi <degarasu@gmail.com>
2026-02-09 21:55:16 +0900

    [update] Remove explicit scene fade time overrides

--- demo/scene_0.gd
+++ demo/scene_0.gd
@@ -20,7 +20,5 @@ func _on_begin_fake_loading_button_pressed() -> void:
 
 func _on_load_additional_button_button_up() -> void:
 	var opts := SceneLoadOptions.new()
-	opts.fade_out_time = 0
-	opts.fade_in_time = 0
 	opts.node_name = "HUD"
 	SceneManager.add_scene(Scenes.Id.ADDITIONAL_0, opts)
