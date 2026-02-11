b0115fde5fd3be75d22713dfd45c47209bad3796
Degarashi <degarasu@gmail.com>
2026-02-11 16:25:11 +0900

    [add] Ring buffer for scene IDs

---
+++ addons/scene_manager/data_store/ring_buffer_scene_id.gd
@@ -0,0 +1,38 @@
+extends RefCounted
+
+const _RING_BUFFER = preload("./ring_buffer.gd")
+var _buffer := _RING_BUFFER.new()
+
+
+func _init(capacity: Scenes.Id = _RING_BUFFER.DEFAULT_RING_SIZE) -> void:
+	_buffer.set_capacity(capacity)
+
+
+func push(value: Scenes.Id) -> void:
+	_buffer.push(value)
+
+
+func pop() -> Scenes.Id:
+	var v = _buffer.pop()
+	if v == null:
+		return 0
+	return v
+
+
+func size() -> Scenes.Id:
+	return _buffer.size()
+
+
+func capacity() -> Scenes.Id:
+	return _buffer.capacity()
+
+
+func clear() -> void:
+	_buffer.clear()
+
+
+func get_all_items() -> Array[Scenes.Id]:
+	var arr: Array[Scenes.Id] = []
+	for v in _buffer.get_all_items():
+		arr.append(v)
+	return arr
