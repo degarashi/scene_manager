5483add284a88e5c77665c89bcdd3f480245de53
Degarashi <degarasu@gmail.com>
2026-02-20 17:24:54 +0900

    [update] Category activation refinement and assertion
    Refined `activate` method to ensure `_activate` is called after category name setup.
    Added `is_inside_tree()` assertion for robust initialization state check.

--- addons/scene_manager/editor/scene_tab/category_base.gd
+++ addons/scene_manager/editor/scene_tab/category_base.gd
@@ -20,17 +20,19 @@ func _activate() -> void:
 
 
 func activate(category_id: int) -> void:
+	assert(is_inside_tree())
 	_category_id = category_id
 
 	if category_id == ResourceUID.INVALID_ID:
 		name = _C.ALL_CATEGORY_NAME
+		_activate()
 	else:
 		# get category-name from id
 		var recv: Array[SMgrCategoryData]
 		_ebus_editor.get_category_by_id.emit(recv, category_id)
 		if not recv.is_empty():
 			name = recv[0].name
-	_activate()
+			_activate()
 
 
 func _on_remove_list_button_up() -> void:
