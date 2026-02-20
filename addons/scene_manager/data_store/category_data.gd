ca8ff731f16ba7e1fd8cf288cb74bddc2fb9584b
Degarashi <degarasu@gmail.com>
2026-02-20 22:41:24 +0900

    [update] Category data property setter implementation
    Implement custom setters for 'name' and 'layer_priority' properties.

--- addons/scene_manager/data_store/category_data.gd
+++ addons/scene_manager/data_store/category_data.gd
@@ -1,8 +1,18 @@
+@tool
 class_name SMgrCategoryData
 extends Resource
 
-@export var name: String
-@export var layer_priority: int = 1
+@export var name: String:
+	set(value):
+		if name != value:
+			name = value
+			emit_changed()
+
+@export var layer_priority: int = 1:
+	set(value):
+		if layer_priority != value:
+			layer_priority = value
+			emit_changed()
 
 
 func _init(p_name: String = "") -> void:
