3397ec186cb76e89d1298bcece8d7b769c733cd2
Degarashi <degarasu@gmail.com>
2026-02-21 14:18:12 +0900

    [add] Category layer priority spinbox control
    Adds a SpinBox to the category edit widget for modifying the
    selected category's layer priority.

--- addons/scene_manager/editor/widget/category_edit.gd
+++ addons/scene_manager/editor/widget/category_edit.gd
@@ -2,19 +2,39 @@
 extends VBoxContainer
 
 @export var _ebus_editor: SMgrEbusEditor
+var _category_id: int
+@onready var _layer_priority_box: SpinBox = %LayerPriorityBox
 
 
 func _ready() -> void:
 	_ebus_editor.on_category_selected.connect(_on_category_selected)
 
 
-func _on_category_selected(id: int) -> void:
+func _fetch_category(id: int) -> SMgrCategoryData:
 	var recv: Array[SMgrCategoryData]
 	_ebus_editor.get_category_by_id.emit(recv, id)
 	if recv.is_empty():
+		return null
+	return recv[0]
+
+
+func _on_category_selected(id: int) -> void:
+	if not is_inside_tree():
 		return
-	var cat := recv[0]
+
+	_category_id = id
+	var cat := _fetch_category(id)
 	if cat:
 		self.visible = true
+		_layer_priority_box.value = cat.layer_priority
 	else:
 		self.visible = false
+
+
+func _on_layer_priority_box_value_changed(value: float) -> void:
+	var cat := _fetch_category(_category_id)
+	if not cat:
+		return
+
+	cat.layer_priority = int(value)
+	cat.emit_changed()
