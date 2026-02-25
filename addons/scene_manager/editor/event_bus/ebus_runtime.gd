93fafc0a1a8532cccbaf41286e44c83b15d40f2c
Degarashi <degarasu@gmail.com>
2026-02-25 15:04:13 +0900

    [add] Scene layer ID lookup support
    Introduces functionality to retrieve `SMgrSceneLayer` instances using their unique scene ID.

--- addons/scene_manager/editor/event_bus/ebus_runtime.gd
+++ addons/scene_manager/editor/event_bus/ebus_runtime.gd
@@ -2,5 +2,6 @@ class_name SMgrEbusRuntime
 extends SMgrEventBusBase
 
 signal pause_threshold_changed(priority: int)
+signal get_scene_by_id(recv: Array[SMgrSceneLayer], scene_id: Scenes.Id)
 signal get_scene_by_name(recv: Array[SMgrSceneLayer], scene_name: String)
 signal process_scene_layer(proc: Callable)
