5f639b3d1a8a1f191d3972b9270c09e5de4f5993
Degarashi <degarasu@gmail.com>
2026-03-08 17:47:55 +0900

    [update] Signal connection robustness, method relocation
    Refactor signal connections using `_AF.connect_if_not_connected` for improved robustness.

--- addons/scene_manager/data_store/debounced_resource.gd
+++ addons/scene_manager/data_store/debounced_resource.gd
@@ -1,9 +1,14 @@
 class_name DebouncedResource
 extends Resource
 
+# ------------- [Signal] -------------
 ## Signal emitted when the debounce delay has successfully completed.
 signal data_changed_debounced
 
+# ------------- [Constants] -------------
+const _AF = preload("uid://dlgh4u64a7qxk")
+
+# ------------- [Private Variable] -------------
 ## Internal debouncer instance
 var _debouncer: DebouncerRC
 
@@ -11,6 +16,7 @@ var _debouncer: DebouncerRC
 var _debounce_time: float = 0.5
 
 
+# ------------- [Private Method] -------------
 ## Initializes the debouncer and starts monitoring change notifications.
 ## Expected to be called in the _init method of inherited classes.
 func _init_debouncer(p_delay: float = 0.5) -> void:
@@ -18,19 +24,10 @@ func _init_debouncer(p_delay: float = 0.5) -> void:
 
 	if _debouncer == null:
 		_debouncer = DebouncerRC.new(_debounce_time, true)
-		_debouncer.timeout.connect(_on_debounce_timeout)
+		_AF.connect_if_not_connected(_debouncer.timeout, _on_debounce_timeout)
 
 	# Hook into the Resource's own change notification (emit_changed) to trigger the debouncer
-	if not changed.is_connected(_on_resource_changed):
-		changed.connect(_on_resource_changed)
-
-
-## Updates the debounce delay time.
-## If the debouncer is already initialized, it updates its internal delay.
-func set_delay(p_delay: float) -> void:
-	_debounce_time = p_delay
-	if _debouncer:
-		_debouncer.set_delay(p_delay)
+	_AF.connect_if_not_connected(changed, _on_resource_changed)
 
 
 ## Handler for when the Resource emits a changed signal.
@@ -42,6 +39,15 @@ func _on_resource_changed() -> void:
 ## Called when the debouncer timer finishes.
 func _on_debounce_timeout() -> void:
 	data_changed_debounced.emit()
+
+
+# ------------- [Public Method] -------------
+## Updates the debounce delay time.
+## If the debouncer is already initialized, it updates its internal delay.
+func set_delay(p_delay: float) -> void:
+	_debounce_time = p_delay
+	if _debouncer:
+		_debouncer.set_delay(p_delay)
 
 
 ## Cleanup the debouncer instance and connections.
