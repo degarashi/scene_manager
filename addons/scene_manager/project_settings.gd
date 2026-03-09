4708dddcaea712e3dd7a6c15b8d034e988a2f86c
Degarashi <degarasu@gmail.com>
2026-03-09 12:00:03 +0900

    [add] Log enable setting change signal
    Introduce `on_enable_log_changed` signal.

--- addons/scene_manager/project_settings.gd
+++ addons/scene_manager/project_settings.gd
@@ -3,6 +3,7 @@ class_name SMgrProjectSettings
 extends SMgrResource
 
 signal on_auto_save_changed(enable: bool)
+signal on_enable_log_changed(enable: bool)
 
 const DEFAULT_DATA_DIR = "res://scene_manager_data"
 const DEFAULT_SCENES_FILENAME = "scenes.gd"
@@ -10,6 +11,7 @@ const DEFAULT_PATH_TO_SCENES := DEFAULT_DATA_DIR + "/" + DEFAULT_SCENES_FILENAME
 const DEFAULT_SCENES_DATA_FILENAME = "scenes_data.tres"
 const DEFAULT_PLAY_OUT_TIME: float = 1
 const DEFAULT_PLAY_IN_TIME: float = 1
+const _AF = preload("uid://dlgh4u64a7qxk")
 
 
 # Setting Paths
@@ -88,6 +90,10 @@ var enable_log: bool:
 		if enable_log != value:
 			ProjectSettings.set_setting(Property.ENABLE_LOG, value)
 			_save()
+			_last_enable_log = value
+			on_enable_log_changed.emit(value)
+
+var _last_enable_log: bool = false
 
 
 func _ensure_data_dir_exists(dir_path: String) -> void:
@@ -102,7 +108,18 @@ func _save() -> void:
 		push_error("SceneManager: Failed to _save ProjectSettings (Error code: %d)" % error)
 
 
+## Handler to detect changes made to settings from external sources like the Project Settings window
+func _on_project_settings_changed() -> void:
+	var current_log := enable_log
+	if current_log != _last_enable_log:
+		_last_enable_log = current_log
+		on_enable_log_changed.emit(current_log)
+
+
 func setup_project_settings() -> void:
+	# Connect signals (monitor direct changes from the editor)
+	_AF.connect_if_not_connected(ProjectSettings.settings_changed, _on_project_settings_changed)
+
 	_ensure_data_dir_exists(scene_path.get_base_dir())
 	# Structured configuration using constant keys
 	var settings: Dictionary[String, Dictionary] = {
@@ -178,6 +195,8 @@ func setup_project_settings() -> void:
 		if s.get(Key.RESTART, false) as bool:
 			ProjectSettings.set_restart_if_changed(path, true)
 
-	# Persist changes to project.godot only if needed
+	# Cache the initial state
+	_last_enable_log = enable_log
+
 	if needs_save:
 		_save()
