d4cc40ab37f893adf582f19357041b81dabb652d
Degarashi <degarasu@gmail.com>
2026-05-16 15:34:32 +0900

    [update] IFadeOutNotify += on_fade_out_start()

--- addons/scene_manager/interfaces/i_fade_out_notify.gd
+++ addons/scene_manager/interfaces/i_fade_out_notify.gd
@@ -3,10 +3,14 @@
 #  This file was generated from an .ifc definition.
 # ============================================================
 
-# Generated at: 2026-05-16 14:52:27
+# Generated at: 2026-05-16 15:33:45
 
 class_name IFadeOutNotify
 extends InterfaceBase
+
+func on_fade_out_start() -> void:
+	assert(is_valid(), "[Interface] Accessing freed instance")
+	_impl.on_fade_out_start()
 
 func on_fade_out_end() -> void:
 	assert(is_valid(), "[Interface] Accessing freed instance")
