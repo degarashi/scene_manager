b64ea89ae71cf88ecc20a476242c05756e70cb08
Degarashi <degarasu@gmail.com>
2026-05-18 19:00:31 +0900

    [add] signal _instance_check

--- addons/scene_manager/editor/event_bus/ebus_runtime.gd
+++ addons/scene_manager/editor/event_bus/ebus_runtime.gd
@@ -5,3 +5,6 @@ signal pause_threshold_changed(priority: int)
 signal get_scene_by_id(recv: Array[SMgrSceneLayer], scene_id: Scenes.Id)
 signal get_scene_by_name(recv: Array[SMgrSceneLayer], scene_name: String)
 signal process_scene_layer(proc: Callable)
+
+## Internal signal used to detect multiple SceneManager instances.
+signal _instance_check
