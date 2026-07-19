85fc5d5c79ed122aa7bcad2e23db1c525410036d
Degarashi <degarasu@gmail.com>
2026-07-20 01:12:23 +0900

    [feat] highlight Save button in blue when there are unsaved changes

--- addons/scene_manager/editor/widget/save_button.gd
+++ addons/scene_manager/editor/widget/save_button.gd
@@ -3,11 +3,25 @@ extends Button
 
 @export var _ebus_editor: SMgrEbusEditor
 var _ps := preload("uid://dn6eh4s0h8jhi")  # project_settings.tres
+var _dirty_style: StyleBoxFlat
 
 
 func _ready() -> void:
 	_ebus_editor.on_dirty_flag_changed.connect(_adapter_func)
 	_ps.on_auto_save_changed.connect(_adapter_func)
+
+	_dirty_style = StyleBoxFlat.new()
+	_dirty_style.bg_color = Color(0.2, 0.5, 1.0, 0.2)
+	_dirty_style.border_color = Color(0.2, 0.5, 1.0, 0.5)
+	_dirty_style.border_width_left = 1
+	_dirty_style.border_width_top = 1
+	_dirty_style.border_width_right = 1
+	_dirty_style.border_width_bottom = 1
+	_dirty_style.corner_radius_top_left = 3
+	_dirty_style.corner_radius_top_right = 3
+	_dirty_style.corner_radius_bottom_right = 3
+	_dirty_style.corner_radius_bottom_left = 3
+
 	_update_state.call_deferred()
 
 
@@ -23,3 +37,8 @@ func _update_state() -> void:
 
 	var dirty := recv[0]
 	disabled = _ps.auto_save or not dirty
+
+	if dirty and not _ps.auto_save:
+		add_theme_stylebox_override("normal", _dirty_style)
+	else:
+		remove_theme_stylebox_override("normal")
